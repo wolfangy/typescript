61afe960f8feee3217e65cebd8f83ba8acf37790
Juanyong YANG <juanyong.yang@gmail.com>
2022-08-15 11:44:38 -0400

    updated

--- programming-typescript/src/ch-fp-ts/option-test.ts
+++ programming-typescript/src/ch-fp-ts/option-test.ts
@@ -1,5 +1,5 @@
 import { pipe } from "fp-ts/lib/function";
-import {Option, match } from "fp-ts/lib/Option"
+import { Option, match } from "fp-ts/lib/Option"
 import { loadDatabase } from "../ch4-functions/ch4-functions";
 
 const usr1 = loadDatabase(3);
@@ -7,7 +7,7 @@ const usr2 = loadDatabase(4);
 
 const report = (usr: Option<string>): void => pipe(usr,
     match(
-        () => console.log('value is not there'), 
+        () => console.log('value is not there'),
         (v: string) => console.log('value loaded', v)));
 
 report(usr1);
